5c246e4de41c211613873354c82d522beab96333
ZhouJune <574089398@qq.com>
2019-12-03 09:11:37 +0800

    Updated adminx and everything

--- data/views.py
+++ data/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Player, Record, High_school
+from .models import Player, Record, High_school,Accolade
 from .forms import PlayerForm
 
 
@@ -37,12 +37,26 @@ def search(request):
 
         league_id = request.POST['League']
         position_id = request.POST['Positions']
+        year_id = request.POST['Starer_Year']
+        all_id = request.POST['All_Conference_Year']
 
         if position_id and position_id != '0':
             players = players.filter(id__in = Record.objects.filter(Position1=int(position_id)).values_list('Player_id'))
 
         if league_id and league_id != '0':
             players = players.filter(College__College_League=int(league_id)).order_by('id')
+
+        if year_id and year_id != '0':
+            from django.db.models import Count
+            starter_record = Record.objects.filter(Is_starter=True).values('Player_id').annotate(total = Count('Player_id')).filter(total = year_id)
+            players = players.filter(id__in = starter_record.values_list('Player_id'))
+
+
+        if all_id and all_id != '0':
+            from django.db.models import Count
+            all_acc = Accolade.objects.values('Player_id').annotate(total = Count('Player_id')).filter(total = all_id)
+            players = players.filter(id__in = all_acc.values_list('Player_id'))
+
 
         HS = High_school.objects.filter(id__in=players.values_list('High_School_id'))
 
